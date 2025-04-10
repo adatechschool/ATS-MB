aa9c10f3e35ebd745bb21e99f2872624b20ff726
Sebus83 <sebastien.herbiet@gmail.com>
2025-04-10 14:57:56 +0200

    test2

--- devshare-app/src/App.vue
+++ devshare-app/src/App.vue
@@ -4,14 +4,16 @@ import Homepage from './components/Homepage.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <Homepage msg="Vite + Vue" />
   </div>
-  <Homepage msg="Vite + Vue" />
 </template>
 
 <style scoped>
