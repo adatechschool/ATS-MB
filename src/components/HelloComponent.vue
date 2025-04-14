a04d21ffa415e68420965766775d0388d6c02a09
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-14 12:45:02 +0200

    style: 💄  undo some nasty styles

--- src/components/HelloComponent.vue
+++ src/components/HelloComponent.vue
@@ -1,11 +1,13 @@
 <!-- src\components\HelloComponent.vue -->
 
 <template>
-  <div class="rounded bg-green-200 p-4 shadow">
+  <div class="rounded p-4 shadow">
     <p class="text-xl">Hello from HelloComponent!</p>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+/* You can add additional styling if needed */
+</style>
--- src/components/WorldComponent.vue
+++ src/components/WorldComponent.vue
@@ -1,11 +1,13 @@
 <!-- src\components\WorldComponent.vue -->
 
 <template>
-  <div class="rounded bg-yellow-200 p-4 shadow">
+  <div class="rounded p-4 shadow">
     <p class="text-xl">Greetings from WorldComponent!</p>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+/* You can add additional styling if needed */
+</style>
